5da4bce95bd944039662d65316ae98693cdfdb0d
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-01-06 16:57:03 -0500

    feat: add constructor parameters for test_resource class constructors

--- priority_queue_test/test_resource.helper.h
+++ priority_queue_test/test_resource.helper.h
@@ -4,15 +4,34 @@
 
 class test_resource
 {
+private:
+	std::string test_string;
+	unsigned test_number;
+
 public:
 	static unsigned instances_copied;
 	static unsigned instances_created;
 	static unsigned instances_moved;
 	static unsigned instances_destroyed;
 
-	test_resource() { ++instances_created; }
-	test_resource(const test_resource& rhs) { ++instances_copied; }
-	test_resource(test_resource&& rhs) noexcept { ++instances_moved; }
+	test_resource(const std::string test_string_ = "test", unsigned test_number_  = 0)
+		: test_string(test_string_), test_number(test_number_)
+	{
+		++instances_created;
+	}
+
+	test_resource(const test_resource& rhs)
+		: test_string(rhs.test_string), test_number(rhs.test_number)
+	{
+		++instances_copied;
+	}
+
+	test_resource(test_resource&& rhs) noexcept 
+		: test_string(std::move(rhs.test_string)), test_number(rhs.test_number)
+	{ 
+		++instances_moved;
+	}
+
 	~test_resource() { --instances_destroyed; }
 };
 
