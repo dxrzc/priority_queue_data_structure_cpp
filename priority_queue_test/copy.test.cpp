4d84e75cd7520b2e91329d3496c3eb88fdf66836
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-01-07 11:49:30 -0500

    feat: copy constructor test

---
+++ priority_queue_test/copy.test.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "priority_queue.h"
+#include "test_resource.helper.h"
+
+TEST(copy_constructor, should_update_nelms)
+{
+	PriorityQueue<unsigned, std::string, std::greater> pqueue;
+	pqueue.push(10, "cat");
+	pqueue.push(12, "dog");
+	pqueue.push(98, "horse");
+
+	PriorityQueue<unsigned, std::string, std::greater> test_queue = pqueue;
+
+	EXPECT_EQ(test_queue.size(), pqueue.size());
+}
+
+TEST_F(constructor_operations_test, should_copy_every_element)
+{
+	PriorityQueue<unsigned, test_resource, std::greater> pqueue;
+	// 3 created
+	pqueue.emplace(10, "cat", 0);
+	pqueue.emplace(12, "dog", 0);
+	pqueue.emplace(98, "horse", 0);
+
+	// 3 copied
+	PriorityQueue<unsigned, test_resource, std::greater> test_queue = pqueue;
+
+	EXPECT_EQ(test_resource::instances_created, 3);
+	EXPECT_EQ(test_resource::instances_copied, 3);
+}
